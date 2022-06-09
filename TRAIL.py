1553b90be3b2807906691b00d8c030f587bd1914
Imtiaz Aradin <97676228+Aradin110@users.noreply.github.com>
2022-06-09 10:43:59 +0500

    Update TRAIL.py

--- TRAIL.py
+++ TRAIL.py
@@ -8,7 +8,7 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
-    from KING import Subscription
+    from king import Subscription
     Subscription()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
