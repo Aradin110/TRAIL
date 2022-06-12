9ae409c2ac890798132d30a91cb0b662d95910fa
Imtiaz Aradin <97676228+Aradin110@users.noreply.github.com>
2022-06-12 21:37:34 +0500

    Update Fuck.py

--- Fuck.py
+++ Fuck.py
@@ -1,2 +1,5 @@
-import os, sys                                                        try:                                                                      __import__("jfuck").__niki____()
-except Exception as e:                                                    exit(str(e))
+import os, sys                                                        
+try:                                                                      
+    __import__("jfuck").__niki____()
+except Exception as e:                                                    
+    exit(str(e))
